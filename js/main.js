aa39ba70b37b21dce192888b4dfbe6c7831cf427
hta-pete <pkatsaboulas@hightoweragency.com>
2023-08-24 06:40:46 -0500

    address resize firing on mobile ios

--- js/main.js
+++ js/main.js
@@ -3,41 +3,20 @@ gsap.registerPlugin(Draggable)
 gsap.registerPlugin(ScrollTrigger)
 
 // Loader
-var num        = 0;
-var loader     = document.getElementById("load-stuff")
-var loader_num = document.getElementById("load-num");
-var content    = document.getElementById("work ");
+var loader = document.getElementById("load-stuff")
 
 document.onreadystatechange = function () {
   if (document.readyState !== "complete") {
-      loader.style.visibility = "visible";
-      imgLoad(document.images[num]);
+      //loader.style.visibility = "visible";
+      console.log("loading...")
   } else {
     window.scrollTo(0, 0);
-    loader.style.display = "none";
+    //loader.style.display = "none";
     gsap.to([".headline", ".cta", ".work"], {y: 0, opacity:1, duration:0, stagger: 0.1});
+
+    console.log("loaded!")
   }
 };
-
-
-function imgLoad(img) {
-	var img_length = document.images.length ;
-	img.src = img.dataset.loadsrc ||img.getAttribute("src");
-
-  setTimeout (function(){ 
-      loader_num.style.display = "block";
-      loader_num.textContent = Math.ceil((num)/(img_length)*100);
-      num ++;
-      if(num < img_length){
-        imgLoad(document.images[num]);	
-      }
-	},100)
-}
-
-
-
-
-
 
 
 // Lenis smooth scroll
@@ -53,7 +32,7 @@ requestAnimationFrame(raf)
 let styles      = getComputedStyle(document.documentElement),
     largeSlide  = parseInt(styles.getPropertyValue('--large-slide'), 10), 
     smallSlide  = parseInt(styles.getPropertyValue('--small-slide'), 10),
-    projects = document.querySelectorAll(".project")
+    projects    = document.querySelectorAll(".project")
     
 projects.forEach( (project) => {
     let sections      = project.querySelectorAll(".project__slide"),
@@ -90,7 +69,6 @@ projects.forEach( (project) => {
           x: gsap.utils.snap(smallSlide / sections.length * sections.offsetWidth )
         }
     });
-
 })
 
 if(window.innerWidth > 600) {
@@ -103,13 +81,21 @@ if(window.innerWidth > 600) {
   });
 }
 
-window.onresize = () => {
-  largeSlide  = parseInt(styles.getPropertyValue('--large-slide'), 10) 
-  smallSlide  = parseInt(styles.getPropertyValue('--small-slide'), 10)
 
-  gsap.to(".project__slides", {x: 0, duration: 0});
+ 
+let windowWidthResize = window.innerWidth;
+
+window.onresize = () => {   
+    if (window.innerWidth != windowWidthResize) {
+        // Update the window and slide widths
+        windowWidthResize = window.innerWidth;
+        largeSlide  = parseInt(styles.getPropertyValue('--large-slide'), 10) 
+        smallSlide  = parseInt(styles.getPropertyValue('--small-slide'), 10)
+
+        gsap.to(".project__slides", {x: 0, duration: 0});
+        
+    }        
 }
-
 
 
 
